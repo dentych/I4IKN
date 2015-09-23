a9028e05776d75f5dd3da91d019c2b26dabea30b
dentych <dennis@tychsen.me>
2015-09-23 20:34:15 +0200

    Pushed SocketProgram exercise

---
+++ SocketProgramming/SocketClient/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+
+namespace SocketClient {
+    class Program {
+        static void Main(string[] args) {
+            try {
+                // Create a TcpClient.
+                // Note, for this client to work you need to have a TcpServer 
+                // connected to the same address as specified by the server, port
+                // combination.
+                int port = 9000;
+                string filename, ip;
+                
+                if (args.Length < 2) {
+                    Console.WriteLine("You should open the program like ./SocketClient.exe <ip> <filename>");
+                    return;
+                }
+
+                ip = args[0];
+                filename = args[1];
+                TcpClient client = new TcpClient(ip, port);
+
+                // Translate the passed message into ASCII and store it as a Byte array.
+                byte[] data = System.Text.Encoding.ASCII.GetBytes(filename);
+
+                // Get a client stream for reading and writing.
+                //  Stream stream = client.GetStream();
+                NetworkStream stream = client.GetStream();
+
+                // Send the message to the connected TcpServer. 
+                stream.Write(data, 0, data.Length);
+
+                // Receive the TcpServer.response.
+                // Buffer to store the response bytes.
+                data = new byte[1000];
+
+                // Remove path from filename.
+                string file = Path.GetFileName(filename);
+                // Create BinaryWriter to write the read data to a file.
+                var writer = new BinaryWriter(File.Open(file, FileMode.Create));
+                int i, counter = 1;
+                while (true) {
+                    i = stream.Read(data, 0, data.Length);
+                    writer.Write(data);
+
+                    Console.WriteLine("#{0} - {1} bytes", counter, i);
+
+                    if (i < 1000) {
+                        break;
+                    }
+
+                    counter++;
+                }
+
+                // Close everything.
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException e) {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e) {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+
+            Console.WriteLine("\n Press Enter to continue...");
+            Console.Read();
+        }
+    }
+}
---
+++ SocketProgramming/SocketServer/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace SocketServer {
+    class Program {
+        public static void Main() {
+            TcpListener server = null;
+            try {
+                // Set the TcpListener on port 13000.
+                Int32 port = 9000;
+                IPAddress localAddr = IPAddress.Parse("0.0.0.0");
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for client requests.
+                server.Start();
+
+                // Buffer for reading data
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                // Enter the listening loop.
+                Console.Write("Waiting for a connection... ");
+
+                // Perform a blocking call to accept requests.
+                // You could also user server.AcceptSocket() here.
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Connected!");
+
+                data = null;
+
+                // Get a stream object for reading and writing
+                NetworkStream stream = client.GetStream();
+
+                int i = stream.Read(bytes, 0, bytes.Length);
+
+                // Translate data bytes to a ASCII string.
+                data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                Console.WriteLine("Received: {0}", data);
+
+                // Check if file exists
+                if (File.Exists(data)) {
+                    var reader = new BinaryReader(File.Open(data, FileMode.Open));
+
+                    int counter = 1;
+                    while (true) {
+                        byte[] array = reader.ReadBytes(1000);
+                        stream.Write(array, 0, array.Length);
+
+                        Console.WriteLine("#{0} - {1} bytes", counter, array.Length);
+
+                        if (array.Length < 1000)
+                            break;
+
+                        counter++;
+                    }
+                }
+
+                // Shutdown and end connection
+                client.Close();
+            }
+            catch (SocketException e) {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+        }
+    }
+}
